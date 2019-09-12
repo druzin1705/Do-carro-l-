060916b373c30fb4ce8df4a8baef65d3642ce536
druzin1705 <matheus.seosgomes@gmail.com>
2019-09-12 12:11:32 -0300

    Commitando atualizações na classe principal

--- src/banco/Principal.java
+++ src/banco/Principal.java
@@ -5,13 +5,28 @@ package banco;
 public class Principal {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
        Banco b = new Banco();
+       b.getCliente();
+       b.getCodBanco();
+       b.getFuncionario();
+       b.getNome();
        
        Cliente c = new Cliente();
        c.getBanco();
        c.getCodCliente();
        c.getCredito();       
+       
+       Funcionario f = new Funcionario();
+       f.getCodFuncionario();
+       f.getSetor();
+       f.getBanco();
+       
+       Pessoa p = new Pessoa(){};
+       p.getNome();
+       p.getIdade();
+       p.getCpf();
     }
     
 }
